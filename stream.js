63c8912e175357421ff7e6025f5fe5f169d53877
Ewetumo Alexander <trinoxf@gmail.com>
2013-04-01 01:23:23 +0100

    changing a few jazz tests

--- stream.js
+++ stream.js
@@ -1,9 +1,8 @@
 var	ts = require('tsk').ToolStack,
-	util = ts.Utility,
 	sm = module.exports = {};
 
-sm.Stream = require('./lib/stream')(ts,util);
-sm.opStream = require('./lib/opstream')(sm.Stream,ts,util);
+sm.Stream = require('./lib/stream')(ts);
+sm.opStream = require('./lib/opstream')(sm.Stream,ts);
 // sm.opstream = require('./lib/opstream')(sm.stream);
 // sm.opstream = require('./lib/opstream')(sm.stream);
 // sm.opstream = require('./lib/opstream')(sm.stream);
